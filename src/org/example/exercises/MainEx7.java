aba47bb6f1fd870bc77bc0c3182a5bd000e71f3c
BvSiT <34773187+BvSiT@users.noreply.github.com>
2021-11-16 16:43:16 +0100

    Solution ex. 7

---
+++ src/org/example/exercises/MainEx7.java
@@ -0,0 +1,11 @@
+package org.example.exercises;
+
+import java.util.Arrays;
+
+public class MainEx7 {
+    public static void main(String[] args) {
+        int player=3;
+        System.out.println(String.format("Player %d has won!",player));
+        System.out.println("-".repeat(10));
+    }
+}
